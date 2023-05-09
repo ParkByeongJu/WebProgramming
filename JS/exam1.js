74bb2cf59a1ec2bff8f18835c39e6333fd2bd27b
ParkByeongJu <bj4906@gmail.com>
2023-05-09 13:13:14 +0900

    이름 입력받고 출력하기

--- JS/exam1.js
+++ JS/exam1.js
@@ -2,7 +2,12 @@ function exam1() {
   var text = prompt("이름을 입력해주세요");
   if (text === null) {
     var text = prompt("이름을 입력해주세요");
-    if (text === null) var text = prompt("이름을 입력해주세요");
-    else alert(text + "님 안녕하세요");
-  } else alert(text + "님 안녕하세요");
+    if (text === null) {
+      var text = prompt("이름을 입력해주세요");
+    } else {
+      alert(text + "님 안녕하세요");
+    }
+  } else {
+    alert(text + "님 안녕하세요");
+  }
 }
